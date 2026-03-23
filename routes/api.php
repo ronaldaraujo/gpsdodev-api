05e6e69ae00765e2eb720270b7a1e578d93da5e2
ronaldaraujo <f.ronaldaraujo@gmail.com>
2026-03-23 15:12:42 -0300

    feat: persistência de dados anônimos da bússola

--- routes/api.php
+++ routes/api.php
@@ -3,9 +3,11 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ReportRequestController;
+use App\Http\Controllers\BussolaController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
 Route::post('/reports/request', [ReportRequestController::class, 'store']);
+Route::post('/bussola/submit', [BussolaController::class, 'store']);
